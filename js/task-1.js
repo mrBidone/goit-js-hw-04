1c56cc9c31632f1ba96c0dd9974cbbaa263fb957
mrBidone <ab.bz@ukr.net>
2024-04-27 09:03:03 +0200

    Add completed task #1

--- js/task-1.js
+++ js/task-1.js
@@ -1,20 +1,38 @@
-// Напиши функцію slugify(title), яка приймає заголовок статті, параметр title і 
-// повертає slug, створений із цього рядка.
-// Значенням параметра title будуть рядки, слова яких розділені лише пробілами.
-// Усі символи slug повинні бути в нижньому регістрі.
-// Усі слова slug повинні бути розділені тире.
-// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи.
+// Напиши функцію isEnoughCapacity(products, containerSize), яка обчислює, чи помістяться всі товари в контейнер при пакуванні.
 
-console.log("task-1 ==================================");
+// Функція оголошує два параметри:
 
-function slugify(title) {
+// products — об’єкт, у якому ключі містять назви товарів, а їхні значення — кількість цих товарів. Наприклад, { apples: 2, grapes: 4 }.
+// containerSize — число, максимальна кількість одиниць товарів, яку в себе може вмістити контейнер.
+// Функція має повернути результат перевірки, чи помістяться всі товари в контейнер. Тобто порахувати загальну кількість товарів в об’єкті products і повернути true, якщо вона менше або дорівнює containerSize, і false, якщо ні.
 
-    let slugString = title.toLowerCase().replaceAll(" ", "-");
+// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її викликів.
 
-    return slugString;
+console.log("TASK-1==================================================");
+
+function isEnoughCapacity(products, containerSize) {
+    
+    let sumProducts = 0;
+
+    for (const product of Object.values(products)) {
+        sumProducts += product;
+    }
+    return sumProducts >= containerSize ? false : true;
 }
 
-console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+); // false
